b9d0e836c80d7036bd12327a1afbf90833532ef2
Your Name <you@example.com>
2019-09-23 07:33:08 +0000

    first commit

---
+++ PPMToolFull Stack Java/ppmtool/src/main/java/io/kapila/ppmtool/PpmtoolApplication.java
@@ -0,0 +1,16 @@
+package io.kapila.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtoolApplication.class, args);
+	}
+
+
+	
+
+}
